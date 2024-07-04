a6985b09d6f375a08c79f31047b255cc26b63d7d
Tetyana <tetyana.buhai@gmail.com>
2024-07-04 18:10:40 +0200

    New commit

--- nodeBasics/index.mjs
+++ nodeBasics/index.mjs
@@ -1,3 +1,3 @@
 import chalk from 'chalk';
 
-console.log(chalk.magenta('The statue is a figure of Libertas, the Roman goddess of liberty'))
+console.log(chalk.magenta('The statue is a figure of Libertas, the Roman goddess of liberty!'))
